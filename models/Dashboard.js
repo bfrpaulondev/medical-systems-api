aefa3b6702be12aea4bcc5d664a1f72955d27240
bfrpaulondev <bfrpaulondev@gmail.com>
2025-02-10 16:48:44 +0000

    Delete Dashboard.js

--- models/Dashboard.js
+++
@@ -1,8 +0,0 @@
-// models/Dashboard.js
-const mongoose = require('mongoose');
-
-const dashboardSchema = new mongoose.Schema({
-  metrics: { type: Object },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Dashboard', dashboardSchema);
